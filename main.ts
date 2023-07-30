87c09459aca92292531e9ced039b5d824e871260
blorbb <88137137+blorbb@users.noreply.github.com>
2023-07-30 19:54:04 +1000

    feat: ctrl+a override (select line)

--- main.ts
+++ main.ts
@@ -1,134 +1,99 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	Editor,
+	EditorPosition,
+	EditorSelection,
+	EditorSelectionOrCaret,
+	Plugin,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
+const LINE_START_REGEX = /^\s*(?:- \[.]|-|\*|\+|>|#{1,6}|[0-9]+\.) /;
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class BlockierPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: "select-block",
+			name: "Select Block",
+			hotkeys: [{ modifiers: ["Meta"], key: "a" }],
+			editorCallback(editor: Editor) {
+				console.log("Running command!!");
+				const selections = editor.listSelections();
+				console.log(selections);
+
+				const newSelections: EditorSelectionOrCaret[] = selections.map(
+					(sel) => selectLine(editor, sel)
+				);
+
+				editor.setSelections(newSelections);
+			},
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+/**
+ * Modifies a selection to cover the whole line.
+ *
+ * If the selection is on one line, it will select the whole paragraph,
+ * excluding block prefixes. These include:
+ * - Unordered lists `-`, `+` and `*`
+ * - Numbered lists `1.`, `2.`, ...
+ * - Headings `#`, `##`, ...
+ * - Checkboxes `- [ ]`, `- [x]`, ...
+ * - Quotes `>`
+ */
+function selectLine(
+	editor: Editor,
+	selection: EditorSelection
+): EditorSelection {
+	// set selection start and end (not anchor and head)
+	const [start, end] = orderPositions(selection.anchor, selection.head);
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+	if (start.line !== end.line) {
+		// selection spans multiple lines: select everything in those lines,
+		// including bullets and other stuff that is usually left out.
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+		return {
+			anchor: {
+				ch: 0,
+				line: start.line,
+			},
+			head: {
+				ch: editor.getLine(end.line).length,
+				line: end.line,
+			},
+		};
+	} else {
+		// selection spans one line: don't select parts at the start (if one exists)
+		// TODO: detect if the selection is currently in a
+		// code block. If so, just select the whole line
+
+		const lineNum = start.line;
+		const line = editor.getLine(lineNum);
+		const paragraphStart = line.match(LINE_START_REGEX)?.[0].length ?? 0;
+		return {
+			anchor: {
+				ch: paragraphStart,
+				line: lineNum,
+			},
+			head: {
+				ch: line.length,
+				line: lineNum,
+			},
+		};
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+function orderPositions(
+	anchor: EditorPosition,
+	head: EditorPosition
+): [EditorPosition, EditorPosition] {
+	if (anchor.line < head.line) {
+		return [anchor, head];
+	} else if (anchor.line > head.line) {
+		return [head, anchor];
+		// below are if selection anchor and head are on the same line
+	} else if (anchor.ch < head.ch) {
+		return [anchor, head];
+	} else {
+		return [head, anchor];
 	}
 }
